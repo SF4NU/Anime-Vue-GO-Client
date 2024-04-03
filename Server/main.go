19e5c645ec4155979dcdd94da32f9d2ced2129b8
SF4NU <mariusmarius200121@gmail.com>
2024-04-03 17:45:07 +0200

    modificato ancora una volta il cors

--- Server/main.go
+++ Server/main.go
@@ -19,7 +19,17 @@ func main() {
 	var err error
 	app := fiber.New()
 
-	middleware.Cors(app)
+	app.Use(func(c *fiber.Ctx) error {
+		c.Set("Access-Control-Allow-Origin", "*")
+		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Set("Access-Control-Allow-Credentials", "true")
+		if c.Method() == "OPTIONS" {
+			c.Status(fiber.StatusOK)
+			return nil
+		}
+		return c.Next()
+	})
 
 	app.Post("/register", handlers.CreateUser)
 	app.Post("/login", handlers.LoginUser)
