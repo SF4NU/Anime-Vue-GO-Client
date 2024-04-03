3af360e63dfd90b57a7ce531250474fac981991f
SF4NU <mariusmarius200121@gmail.com>
2024-04-03 17:37:25 +0200

    altra modifica al cors

--- Server/middleware/cors.go
+++ Server/middleware/cors.go
@@ -6,12 +6,10 @@ import (
 
 func Cors(app *fiber.App) {
 	app.Use(func(c *fiber.Ctx) error {
-		if c.Get("Origin") == "https://sf4nu.github.io" {
-			c.Set("Access-Control-Allow-Origin", "https://sf4nu.github.io")
-		}
-		c.Set("Access-Control-Allow-Credentials", "true")
+		c.Set("Access-Control-Allow-Origin", "*")
 		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		c.Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Set("Access-Control-Allow-Credentials", "true")
 		if c.Method() == "OPTIONS" {
 			c.Status(fiber.StatusOK)
 			return nil
